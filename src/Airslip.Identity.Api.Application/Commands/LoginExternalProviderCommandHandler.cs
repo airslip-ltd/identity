c807522d6c6070454106843bd52299891ec9af13
Tom McDonough <tmcdonough@airslip.com>
2021-05-26 06:06:12 +0100

    Rename of encrypted email variable

--- src/Airslip.Identity.Api.Application/Commands/LoginExternalProviderCommandHandler.cs
+++ src/Airslip.Identity.Api.Application/Commands/LoginExternalProviderCommandHandler.cs
@@ -2,7 +2,6 @@
 using Airslip.Common.Types.Failures;
 using Airslip.Identity.Api.Contracts.Responses;
 using Airslip.Identity.MongoDb.Contracts;
-using Airslip.Security;
 using Airslip.Security.Jwt;
 using Airslip.Yapily.Client.Contracts;
 using MediatR;
@@ -36,16 +35,16 @@ namespace Airslip.Identity.Api.Application.Commands
 
         public async Task<IResponse> Handle(LoginExternalProviderCommand command, CancellationToken cancellationToken)
         {
-            string encryptedEmail = Cryptography.GenerateSHA256String(command.Email);
+            //string encryptedEmail = Cryptography.GenerateSHA256String(command.Email);
 
-            _logger.ForContext(nameof(encryptedEmail), encryptedEmail);
+            _logger.ForContext(nameof(command.Email), command.Email);
 
-            User? user = await _userService.GetByEmail(encryptedEmail);
+            User? user = await _userService.GetByEmail(command.Email);
 
             if (user is null)
             {
                 IYapilyResponse response =
-                    await _yapilyApis.CreateUser(encryptedEmail, command.ReferenceId, cancellationToken);
+                    await _yapilyApis.CreateUser(command.Email, command.ReferenceId, cancellationToken);
 
                 switch (response)
                 {
