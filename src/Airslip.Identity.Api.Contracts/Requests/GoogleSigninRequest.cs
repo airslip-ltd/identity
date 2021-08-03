de242803e60958e37215502257992240c42d8ba9
Tom McDonough <tmcdonough@airslip.com>
2021-08-03 16:29:17 +0100

    Added native iOS Google Authentication support

---
+++ src/Airslip.Identity.Api.Contracts/Requests/GoogleSigninRequest.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Airslip.Identity.Api.Contracts.Requests
+{
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public record GoogleSigninRequest
+    {
+        [JsonPropertyName("email")] public string? Email { get; init; }
+        [JsonPropertyName("name")] public string? Name { get; init; }
+        [JsonPropertyName("picture")] public string? PictureUrl { get; init; }
+        [JsonPropertyName("locale")] public string? Locale { get; init; }
+        [JsonPropertyName("family_name")] public string? FamilyName { get; init; }
+        [JsonPropertyName("given_name")] public string? GivenName { get; init; }
+        [Required] public string? RefreshToken { get; init; }
+        [Required] public string? DeviceId { get; init; }
+    }
+}
