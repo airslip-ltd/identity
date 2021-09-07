363aa32bc90a7d01b72393c6c6f5dab3b74a0b1a
Tom McDonough <tmcdonough@airslip.com>
2021-09-07 05:41:16 +0100

    Added endpoint to find yapily user ids

---
+++ src/Airslip.Identity.Api.Contracts/OpenBankingProviders.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Airslip.Identity.Api.Contracts
+{
+    public static class OpenBankingProviders
+    {
+        public static readonly IEnumerable<string> Names = new List<string>() { "Yapily" };
+    }
+}
