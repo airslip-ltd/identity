6f7008958e0f275a7aeb6a03d4c6b0f0ccf4c53a
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-09 15:44:36 +0000

    Updated user list searching

--- src/Airslip.Identity.Api.Application/Implementations/UserService.cs
+++ src/Airslip.Identity.Api.Application/Implementations/UserService.cs
@@ -2,6 +2,7 @@ using Airslip.Common.Auth.Data;
 using Airslip.Common.Auth.Interfaces;
 using Airslip.Common.Auth.Models;
 using Airslip.Common.Repository.Data;
+using Airslip.Common.Repository.Types.Enums;
 using Airslip.Common.Repository.Types.Interfaces;
 using Airslip.Common.Repository.Types.Models;
 using Airslip.Common.Types.Interfaces;
@@ -11,6 +12,7 @@ using Airslip.Identity.Api.Contracts.Entities;
 using Airslip.Identity.Api.Contracts.Models;
 using Serilog;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Airslip.Identity.Api.Application.Implementations
@@ -18,6 +20,7 @@ namespace Airslip.Identity.Api.Application.Implementations
     public class UserService : IUserService
     {
         private readonly IRepository<User, UserModel> _repository;
+        private readonly IEntitySearch<UserModel> _userSearch;
         private readonly IUserManagerService _userManagerService;
         private readonly IUserLifecycle _userLifecycle;
         private readonly ILogger _logger;
@@ -26,12 +29,14 @@ namespace Airslip.Identity.Api.Application.Implementations
         public UserService(
             ITokenDecodeService<UserToken> tokenDecodeService,
             IRepository<User, UserModel> repository,
+            IEntitySearch<UserModel> userSearch, 
             IUserManagerService userManagerService,
             IUserLifecycle userLifecycle,
             ILogger logger)
         {
             _userToken = tokenDecodeService.GetCurrentToken();
             _repository = repository;
+            _userSearch = userSearch;
             _userManagerService = userManagerService;
             _userLifecycle = userLifecycle;
             _logger = logger;
@@ -75,9 +80,17 @@ namespace Airslip.Identity.Api.Application.Implementations
             return await _repository.Get(id);
         }
 
-        public Task<IResponse> Search(EntitySearchQueryModel query)
+        public async Task<IResponse> Search(EntitySearchQueryModel query)
         {
-            throw new NotImplementedException();
+            EntitySearchResponse<UserModel> searchResults = await _userSearch
+                .GetSearchResults<User>(query,
+                    new List<SearchFilterModel>
+                    {
+                        new(nameof(User.EntityId), _userToken.EntityId),
+                        new(nameof(User.AirslipUserType), _userToken.AirslipUserType)
+                    });
+            
+            return searchResults;
         }
     }
 }
