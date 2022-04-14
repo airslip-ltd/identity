920c3bed31a466a789fa21a559521f983b186f26
Graham Whitehouse <gwhitehouse@airslip.com>
2022-04-14 12:46:48 +0100

    Update main.tf

--- env/apim/main.tf
+++ env/apim/main.tf
@@ -33,7 +33,7 @@ locals {
   api_publisher_name  = var.api_publisher_name
   api_publisher_email = var.api_publisher_email
   api_sku_name        = var.api_sku_name
-  api_policy = "./variables/${var.short_environment}/api_policy.xml"
+  api_policy          = "./variables/${var.short_environment}/api_policy.xml"
 }
 
 data "azurerm_client_config" "current" {}
@@ -52,19 +52,19 @@ module "apim" {
   }
 
   app_configuration = {
-    app_id               = local.app_id,
-    app_id_short         = local.short_app_id,
-    short_environment    = local.short_environment,
-    location             = local.location,
-    tags                 = local.tags,
-    api_publisher_name   = local.api_publisher_name,
-    api_publisher_email  = local.api_publisher_email,
-    api_sku_name         = local.api_sku_name,
-    api_custom_domain    = local.hostname,
-    tenant_id            = data.azurerm_client_config.current.tenant_id,
-    deployer_id          = local.deployment_agent_group_id,
-    revision             = local.revision
-    policy  = local.api_policy
+    app_id              = local.app_id,
+    app_id_short        = local.short_app_id,
+    short_environment   = local.short_environment,
+    location            = local.location,
+    tags                = local.tags,
+    api_publisher_name  = local.api_publisher_name,
+    api_publisher_email = local.api_publisher_email,
+    api_sku_name        = local.api_sku_name,
+    api_custom_domain   = local.hostname,
+    tenant_id           = data.azurerm_client_config.current.tenant_id,
+    deployer_id         = local.deployment_agent_group_id,
+    revision            = local.revision
+    policy              = local.api_policy
   }
 
   apis = local.apis
